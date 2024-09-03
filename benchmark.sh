524a3e3beaee943e7bb9ba339947f44a0cc90e3b
Matteo Panara <matteo.panara@gmail.com>
2024-09-03 17:11:21 +0200

    refactor: test changed

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dest="host"
+source="/app/input"
 if [[ $1 == "cached" ]]; then
   dest="cached"
   cd /benchmark/cached
@@ -10,13 +11,32 @@ elif [[ $1 == "default" ]]; then
 elif [[ $1 == "delegated" ]]; then
   dest="delegated"
   cd /benchmark/delegated
+else
+  source="../input"
+  cd ./output
 fi
 
 echo
 echo "Benchmarking $dest..."
 sync
-host_write_speed=$(dd if=/dev/zero of=./tempfile bs=1M count=1024 conv=fsync 2>&1 | tail -1)
-echo "Write speed: $host_write_speed"
-host_read_speed=$(dd if=./tempfile of=/dev/null bs=1M count=1024 2>&1 | tail -1)
-echo "Read speed: $host_read_speed"
-rm ./tempfile
+inputDirectory=$source
+start_time=$(date +%s%3N)
+file_count=0
+for file in "$inputDirectory"/*.txt; do
+  filename=$(basename "$file")
+  inputFile="$inputDirectory/$filename"
+  outputFile=$filename
+  data=$(cat "$inputFile")
+  processedData=$(echo "$data" | tr '[:upper:]' '[:lower:]')
+  echo "$processedData" > "$outputFile"
+  ((file_count++))
+done
+end_time=$(date +%s%3N)
+elapsed_time_ms=$((end_time - start_time))
+if (( elapsed_time_ms > 0 )); then
+  files_per_second=$(($file_count * 1000 / $elapsed_time_ms))
+else
+  files_per_second=0
+fi
+echo "Speed: $files_per_second files per second."
+rm *.txt
