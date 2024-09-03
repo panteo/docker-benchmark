b4ab467b46f56beb9a97aa66c5613fc0dcd88b93
Matteo Panara <matteo.panara@gmail.com>
2024-09-03 09:17:34 +0200

    refactor: from fdatasync to fsync to solve Mac compatibility issue

--- benchmark.sh
+++ benchmark.sh
@@ -15,7 +15,7 @@ fi
 echo
 echo "Benchmarking $dest..."
 sync
-host_write_speed=$(dd if=/dev/zero of=./tempfile bs=1M count=1024 conv=fdatasync 2>&1 | tail -1)
+host_write_speed=$(dd if=/dev/zero of=./tempfile bs=1M count=1024 conv=fsync 2>&1 | tail -1)
 echo "Write speed: $host_write_speed"
 host_read_speed=$(dd if=./tempfile of=/dev/null bs=1M count=1024 2>&1 | tail -1)
 echo "Read speed: $host_read_speed"
